bf89548802bc7d0d8dc29470554bf613ff28b407
Benjamin Bergstein <bennyjbergstein+github@gmail.com>
2019-10-29 21:53:59 +0000

    basic-federation

--- gateway-hue-review/index.js
+++ gateway-hue-review/index.js
@@ -1,1 +1,18 @@
-console.log("TODO: Build Hue Review Gateway");
+const { ApolloServer } = require("apollo-server");
+const { ApolloGateway} = require("@apollo/gateway");
+
+const gateway = new ApolloGateway({
+  serviceList: [
+    { name: "users", url: "http://localhost:4000" },
+    { name: "colors", url: "http://localhost:4002" },
+  ]
+})
+
+const start = async () => {
+  const { schema, executor } = await gateway.load();
+  const server = new ApolloServer({ schema, executor });
+  const { url } = await server.listen(4003);
+  console.log(`Server running ${url}`);
+};
+
+start();
